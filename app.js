653519b23e3a124ca1ac3866cc9d027fc62645fd
StephenAdedayo <alustephenadedayo@gmail.com>
2024-06-03 21:46:14 +0100

    js math and array methods

--- app.js
+++ app.js
@@ -41,7 +41,192 @@
 // console.log(num10)
 
 
-var Shoppingcart = ["bags", "shorts", "shirts", "wallets", "shoes"]
-// Shoppingcart.push("slippers", "belt")
-Shoppingcart.pop()
-console.log(Shoppingcart);
+// let Shoppingcart = ["bags", "shorts", "shirts", "wallets", "shoes"]
+// // Shoppingcart.push("slippers", "belt")
+// Shoppingcart.push("temmy", "dog")
+// console.log(Shoppingcart);
+
+
+// function getBoot2() {
+//     return Math.PI;
+//   }
+  
+//   console.log(getBoot2());
+
+// function getStevepi() {
+//     return Math.pow(26, 9);
+// }
+// console.log(getStevepi());
+
+
+
+// function getEulerNum(){
+//     return Math.E;
+// }
+
+// console.log(getEulerNum());
+
+
+// function getSqrt(){
+//     return Math.SQRT2;
+// }
+// console.log(getSqrt());
+
+// function getSqrt(){
+//     return Math.SQRT1_2;
+// }
+
+// console.log(getSqrt());
+
+// function getlog2e(){
+//     return Math.LOG2E;
+// }
+
+// console.log(getlog2e());
+
+// function getlog10e(){
+//     return Math.LOG10E;
+// }
+// console.log(getlog10e());
+
+// function getNaturallog(){
+//     return Math.LN2;
+// }
+
+// console.log(getNaturallog());
+
+// function getNaturallog(){
+//     return Math.LN10;
+// }
+// console.log(getNaturallog());
+
+// function getAbsvalue(a, b){
+//     return Math.abs(a - b);
+// }
+
+// console.log(getAbsvalue(3,5));
+
+// function getAcos(x, y){
+//     return Math.acos(x/y);
+// }
+
+// console.log(getAcos(3,5));
+
+// function getMathceil(){
+//     return Math.ceil(1.2999);
+// }
+
+// console.log(getMathceil());
+
+// function getMathfloor(){
+//     return Math.floor(9.479);
+// }
+// console.log(getMathfloor());
+
+// var array = [1, 3, 2, 4];
+
+// console.log(Math.max(...array));
+
+// var steve = [1, -2, 3]
+
+// console.log(Math.min(...steve));
+
+// function getRandomInt(){
+//     return Math.random();
+// }
+
+// console.log(getRandomInt());
+
+// function roundupnum(){
+//     return Math.round(1.99);
+// }
+// console.log(roundupnum());
+
+
+// function getsign(){
+//     return Math.sign(3);
+// }
+
+// console.log(getsign());
+
+
+// function getSqrt(a, b){
+//     return Math.sqrt(a*a + b*b);
+// }
+
+// console.log(getSqrt(3, 4));
+
+// function gettruncvalue(){
+//     return Math.trunc(45.988);
+// }
+
+// console.log(gettruncvalue());
+
+
+// array methods
+
+
+// let cart = ["bags", "shoes", "jean", "shorts"]
+// cart.unshift("steve", "ay");
+
+// console.log(cart);
+
+// let shop = ["bags", "shoes", "jean", "shorts"]
+// shop.shift;
+
+// console.log(shop);
+
+// var steve = ["bags", "shoes", "jean", "shorts"]
+
+
+// console.log(steve.toString());
+
+
+// const fruits = ["food", "clothes", "drums"]
+// let fruit = 2;
+
+// console.log(fruits.at(2));
+
+// const stevo = ["food", "clothes", "drums"]
+// let steve = 2;
+
+// console.log(stevo[2]);
+
+
+// var array = [1, 4, 36, 64]
+
+// var map1 = array.map((x) => x * 2);
+// console.log(map1);
+
+
+// const animals = ["goat", "donkey", "camel", "horse", "peacock"]
+// console.log(animals.slice(2,4));
+
+
+// const month = ["jan", "march", "april", "june"]
+// month.splice(1, 0, "feb");
+
+// console.log(month);
+
+// month.splice(4, 1, "may", "june");
+
+// console.log(month);
+
+// const qwerty = ["drt", "fgj", "akt"]
+// console.log(qwerty.length);
+
+
+// const month = ["dec", "mar", "may", "jan"]
+
+// const sortedmonth = month.toSorted();
+// console.log(sortedmonth);
+
+const array = [1, 2, 3, 4, 5, 6];
+
+const even = (element) => element % 2 === 0;
+
+console.log(array.some(even));
+
+
+
+
