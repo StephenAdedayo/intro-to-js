e53642c26a9505862c63c1e49351e5b61f691da1
StephenAdedayo <alustephenadedayo@gmail.com>
2024-06-03 12:55:32 +0100

    intro to javascript

---
+++ app.js
@@ -0,0 +1,47 @@
+// var firstname = "stephen"
+// var jobtitle = "software developer"
+// var accbalance = undefined
+// var disability = null
+// var position = 1
+// var divorced = false
+
+// console.log(23*9);
+
+
+
+// var a = 10
+// var b = 19
+// var c = a + b
+
+
+// var hundred = 100
+// var three = 4
+// var solution = hundred % three
+// console.log(solution);
+
+
+// var age;
+
+// const inecAgeChecker = (age) => {
+//     if(age < 18){
+//         console.log("ineligible to vote")
+//     }
+//     else{
+//         console.log("eligible to vote")
+//     }
+// } 
+
+// inecAgeChecker(12)
+
+
+// var num = 99
+ 
+// var num10 = num+10
+
+// console.log(num10)
+
+
+var Shoppingcart = ["bags", "shorts", "shirts", "wallets", "shoes"]
+// Shoppingcart.push("slippers", "belt")
+Shoppingcart.pop()
+console.log(Shoppingcart);
